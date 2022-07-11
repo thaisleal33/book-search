74b221c9ba953e8d89bcb1774fe790694e57d8f9
Thaís Leal <86724214+thaisleal33@users.noreply.github.com>
2022-07-11 10:25:25 -0300

    Update index.jsx

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import GlobalStyles from '../styles/GlobalStyles';
 import Navbar from '../components/Navbar';
 
-import About from '../pages/About';
-import Footer from '../components/Footer';
-
 function Layout() {
   return (
     <>
